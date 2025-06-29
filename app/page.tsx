034c4964ec61ec30e222a22361bf9b263850aa31
pg-luc <paolucindo@gmail.com>
2025-06-29 17:25:04 +0800

    updated process page. added styling to process page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
+import "../styles/app.css";
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <h1>Process Page</h1>
+      <div className="container">
+        <h2>Select Supplier</h2>
+        <button>Select Supplier</button>
+      </div>
+    </div>
   );
 }
